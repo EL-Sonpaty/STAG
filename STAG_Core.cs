7ffe92a98f9d12ac32fff95f696aa0487fab7666
Sylvain Usai <usai@designtoproduction.com>
2025-06-14 14:07:11 +0200

    set up STAG Core for Ammar

---
+++ STAG_Core.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STAG
+{
+    public class STAG_Core
+    {
+
+        public void SetStage(Guid RhinoId, string NewStage)
+        {
+            // do you thing here
+        }
+
+        public void UpgradeObject(Guid RhinoId)
+        {
+            // upgrade object from previous stage to next 
+        }
+
+        public void DowngradeObject(Guid RhinoId)
+        {
+            // downgrade object from current stage to previous
+        }
+
+
+    }
+}
