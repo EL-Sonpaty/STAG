c663f7cb7c2f8744e89b7c041552fbe1fff8574c
EL-Sonpaty <Desertfox018@gmail.com>
2025-06-14 19:42:28 +0300

    Added an intitial class for the Tracked Elements model

---
+++ Models/TrackedElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STAG.Models
+{
+    internal class TrackedElement
+    {
+        public Guid Id { get; set; }
+        public Dictionary<string, string> UserObjectAttributes { get; set; } = new Dictionary<string, string>();
+
+        // List to store change history
+        public List<string> ChangeHistory { get; set; } = new List<string>();
+
+        // Optionally, add methods to log changes
+        public void LogChange(string change)
+        {
+            ChangeHistory.Add($"{DateTime.Now}: {change}");
+        }
+    }
+}
