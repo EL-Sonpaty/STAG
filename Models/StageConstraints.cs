0c12c1192ecfe7d833e0ecd6ad70866b51d5d2c6
EL-Sonpaty <Desertfox018@gmail.com>
2025-06-15 01:04:16 +0300

    Added Model Class For Stage Constraints

---
+++ Models/StageConstraints.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STAG.Models
+{
+    public class StageConstraints
+    {
+        public int Number { get; set; }
+        public string Text { get; set; }
+        public bool IsGeometricalEditable { get; set; }
+        public bool IsTranslationEditable { get; set; }
+        public bool IsAttributesEditable { get; set; }
+        public bool IsUserTextsEditable { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string AddedBy { get; set; }
+    }
+}
