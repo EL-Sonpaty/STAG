baa6f322775b80d2b4627de28e0c135e5e154128
Sylvain Usai <usai@designtoproduction.com>
2025-06-14 19:03:00 +0200

    added start and stop listening

---
+++ STAG_StartListening.cs
@@ -0,0 +1,34 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using Rhino.UI;
+using STAG.Wrappers;
+using System;
+using System.Collections.Generic;
+
+namespace STAG
+{
+    public class STAG_StartListening : Command
+    {
+        ///<summary>The only instance of this command.</summary>
+        public static STAG_StartListening Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "STAG_StartListening";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            if (STAGPlugin.Instance.StartListening())
+            {
+                return Result.Success;
+            }
+            else
+            {
+                Rhino.RhinoApp.WriteLine("STAG was already listening");
+                return Result.Failure;
+            }
+        }
+    }
+}
---
+++ STAG_StopListening.cs
@@ -0,0 +1,34 @@
+﻿using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using Rhino.UI;
+using STAG.Wrappers;
+using System;
+using System.Collections.Generic;
+
+namespace STAG
+{
+    public class STAG_StopListening : Command
+    {
+        ///<summary>The only instance of this command.</summary>
+        public static STAG_StopListening Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "STAG_StopListening";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            if (STAGPlugin.Instance.StopListening())
+            {
+                return Result.Success;
+            }
+            else
+            {
+                Rhino.RhinoApp.WriteLine("STAG wasn't listening");
+                return Result.Failure;
+            }
+        }
+    }
+}
